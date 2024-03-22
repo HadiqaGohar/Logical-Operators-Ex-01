60283f6b9cc8122370d7a2d4a2c522eeb62f30a0
HadiqaGohar <160311280+HadiqaGohar@users.noreply.github.com>
2024-03-22 08:49:09 -0700

    && statment

---
+++ main.ts
@@ -0,0 +1,19 @@
+//LOGICAL OPERATORS AND => &&
+
+// Question 1
+let num1 = 50;
+let num2 = 30;
+console.log (num1 == num2 && num1 == 20)   
+            //    false   &&  false = false
+
+// Question 2
+let  firstname = "Hadiqa";
+let lastName = "Gohar";
+console.log (firstname != lastName && firstname >= lastName )            
+     //                      true  && true = true
+  
+// Question 3
+let fastFood = "Burger"
+let desiFood = "Biryani"
+console.log (fastFood == desiFood && fastFood <= desiFood)     
+          //                false  &&  true  = false
